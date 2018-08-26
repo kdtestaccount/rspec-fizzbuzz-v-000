173ad7d496f8349173238fc3f260924e4ad90e21
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-08-26 16:24:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz()
-  
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  end
+  elsif int % 5 == 0 
+    "Buzz"
+  else int % 15 = 0 
+    "Fizzbuzz"
+  end
 end
